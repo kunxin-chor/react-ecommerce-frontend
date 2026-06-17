97a5d1faa3a8c5e304d683a768b69f5bdd535760
kunxin-chor <chorkunxin@yahoo.com>
2026-06-17 07:30:04 +0000

    Added form

--- src/RegisterPage.jsx
+++ src/RegisterPage.jsx
@@ -1,11 +1,27 @@
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 export default function RegisterPage() {
 
     const initialValues = {
         name: "",
-        email: ""
+        email: "",
+        password: "",
+        confirmPassword: "",
+        salutation: "Mr",
+        marketingPreferences: [],
+        country: ""
     }
+
+    const validationSchema = Yup.object({
+        name: Yup.string().required("Please enter your name"),
+        email: Yup.string().email("Please ensure you have entered your email correctly").required("Email is complusory"),
+        password: Yup.string().min(8, "Password must have at least 8 characters").required(),
+        confirmPassword: Yup.string().oneOf([
+            Yup.ref("password"),
+            null
+        ], "Passwords must match").required("Please re-enter your password again")
+    })
 
     // event handler for submitting the form
     // two arguments
@@ -20,6 +36,7 @@ export default function RegisterPage() {
         <Formik
             initialValues={initialValues}
             onSubmit={handleSubmit}
+            validationSchema={validationSchema}
         >
 
             {
@@ -33,7 +50,13 @@ export default function RegisterPage() {
                                 id="name"
                                 name="name"
                             />
+                            <ErrorMessage
+                                name="name"
+                                component="div"
+                                className="text-danger"
+                            />
                         </div>
+
                         <div>
                             <label className="form-label">Email:</label>
                             <Field
@@ -42,8 +65,119 @@ export default function RegisterPage() {
                                 id="email"
                                 name="email"
                             />
+                            <ErrorMessage
+                                name="email"
+                                component="div"
+                                className="text-danger"
+                            />
                         </div>
-                        <button className="btn btn-primary m-1">Submit</button>
+
+                        <div>
+                            <label className="form-label">
+                                Password
+                            </label>
+                            <Field
+                                type="password"
+                                name="password"
+                                className="form-control"
+                            />
+                            <ErrorMessage
+                                name="password"
+                                component="div"
+                                className="text-danger"
+                            />
+                        </div>
+
+                        <div>
+                            <label className="form-label">
+                                Confirm Password
+                            </label>
+                            <Field
+                                type="password"
+                                name="confirmPassword"
+                                className="form-control"
+                            />
+                            <ErrorMessage
+                                name="confirmPassword"
+                                component="div"
+                                className="text-danger"
+                            />
+                        </div>
+
+                        <div>
+                            <label className="form-label">Salutation</label>
+                            <div>
+                                <div className="form-check form-check-inline">
+                                    <Field
+                                        type="radio"
+                                        name="salutation"
+                                        value="Mr"
+                                        className="form-check-input"
+                                    />
+                                    <label className="form-check-label">Mr.</label>
+                                </div>
+                                <div className="form-check form-check-inline">
+                                    <Field
+                                        type="radio"
+                                        name="salutation"
+                                        value="Ms"
+                                        className="form-check-input"
+                                    />
+                                    <label className="form-check-label">Ms.</label>
+                                </div>
+
+                                <div className="form-check form-check-inline">
+                                    <Field
+                                        type="radio"
+                                        name="salutation"
+                                        value="Mrs"
+                                        className="form-check-input"
+                                    />
+                                    <label className="form-check-label">Mrs.</label>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div>
+                            <label className="form-label">Marketing Preferences</label>
+                            <div className="form-check">
+                                <Field
+                                    type="checkbox"
+                                    name="marketingPreferences"
+                                    value="1"
+                                    className="form-check-input"
+                                />
+                                <label className="form-check-label">Email</label>
+                            </div>
+                            <div className="form-check">
+                                <Field
+                                    type="checkbox"
+                                    name="marketingPreferences"
+                                    value="2"
+                                    className="form-check-input"
+                                />
+                                <label className="form-check-label">SMS</label>
+                            </div>
+                        </div>
+
+                        <div>
+                            <label className="form-label">Country</label>
+                            <Field
+                                as="select"
+                                className="form-select"
+                                name="country"
+                            >
+                               <option value="">Select Country</option>
+                               <option value="sg">Singapore</option>
+                               <option value="my">Malaysia</option>
+                               <option value="id">Indonesia</option>
+                               <option value="others">Others</option>
+
+                            </Field>
+                        </div>
+
+                        <button type="submit" className="btn btn-primary m-1">Submit</button>
                     </Form>
                 )
             }
